71fb825544e53f0d7fde71848ae03ef1384b03bb
ukonn_admin <UKONN@ukonn-admin-no-Mac-mini.local>
2018-09-12 23:02:22 +0900

    TrivialChange

--- validate.js
+++ validate.js
@@ -7,7 +7,40 @@ const validator = new PokemonDataValidator();
 const jsonStr = fs.readFileSync(dataFilePath);
 const json = JSON.parse(jsonStr);
 
-const validationResult = validator.validate(json);
+const test =[{ no: 75,
+  name: 'ゴローン',
+  form: 'アローラのすがた',
+  isMegaEvolution: false,
+  evolutions: [ 76 ],
+  types: [ 'いわ', 'でんき' ],
+  abilities: [ 'じりょく', 'がんじょう' ],
+  hiddenAbilities: [ 'エレキスキン' ],
+  stats: 
+   { hp: 40,
+     attack: 80,
+     defence: 100,
+     spAttack: 30,
+     spDefence: 30,
+     speed: 20 
+    } 
+},
+{ no: 79,
+  name: 'ヤドン',
+  form: 'アローラのすがた',
+  isMegaEvolution: false,
+  evolutions: [ 80, 199 ],
+  types: [ 'みず', 'エスパー' ],
+  abilities: [ 'どんかん', 'マイペース' ],
+  hiddenAbilities: [ 'さいせいりょく' ],
+  stats: 
+   { hp: 90,
+     attack: 65,
+     defence: 65,
+     spAttack: 40,
+     spDefence: 40,
+     speed: 15 } }
+]
+const validationResult = validator.validate(test);
 
 if(validationResult.isFine) {
   console.log('正常です。エラーはありませんでした。');
