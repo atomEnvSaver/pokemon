296cdf812035343094bd3eb9f88e7e212e304a3e
atomEnvSaver <ukonnsoft@mineo.jp>
2018-09-17 11:56:08 +0900

    returning names arrayfunc

--- validate.js
+++ validate.js
@@ -20,4 +20,28 @@ if(validationResult.isFine) {
 console.log(`正常なポケモンデータ：${validationResult.passedPokemons.length}件`);
 console.log(`異常のあったポケモンデータ：${validationResult.errorPokemons.length}件`);
 
+/** カタカナをひらがなに変換する比較関数
+ * @param {String} src - カタカナ
+ * @returns {String} - ひらがな
+ */
+const katakanaToHiragana = (src) => {
+  return src.replace(/[\u30a1-\u30f6]/g, function(match) {
+      var chr = match.charCodeAt(0) - 0x60;
+      return String.fromCharCode(chr);
+  });
+}
+
+validator.nameList.sort(function(a, b){
+  a = katakanaToHiragana(a.toString());
+  b = katakanaToHiragana(b.toString());
+  if(a < b){
+      return -1;
+  }else if(a > b){
+      return 1;
+  }
+  return 0;
+});
+
+console.log(`名前リスト：${validator.nameList}`);
+
 process.exit(0);
